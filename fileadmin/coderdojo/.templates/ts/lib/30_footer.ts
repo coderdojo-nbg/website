fcb82408d1ef15cbdc006dda0756d22108a829ee
Joschi Kuphal <joschi@kuphal.net>
2016-01-06 22:04:55 +0100

    Added sponsoring information

--- fileadmin/coderdojo/.templates/ts/lib/30_footer.ts
+++ fileadmin/coderdojo/.templates/ts/lib/30_footer.ts
@@ -1,13 +1,18 @@
-lib.footerNavigation = HMENU
+lib.footerNavigation = COA
 lib.footerNavigation {
-    special = directory
-    special.value = 9
-    1 = TMENU
-    1 {
-        NO = 1
-        ACT = 1
-        ACT {
-            ATagParams = class="active"
+    10 = HMENU
+    10 {
+        special = directory
+        special.value = 9
+        1 = TMENU
+        1 {
+            NO = 1
+            ACT = 1
+            ACT {
+                ATagParams = class = "active"
+            }
         }
     }
+    20 = TEXT
+    20.value = <a href="/fileadmin/user_upload/download/CoderDojo_Nuernberg_Sponsoreninformationen.pdf" target="_blank">Sponsoring</a>
 }
