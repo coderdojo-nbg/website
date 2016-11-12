4d4d74df23e83bbd673c594f8e75e784343acc5e
Joschi Kuphal <joschi@kuphal.net>
2016-11-12 14:42:41 +0100

    Added microdata / Microformats

---
+++ fileadmin/coderdojo/.templates/ts/plugins/20_payment.ts
@@ -0,0 +1,4 @@
+plugin.tx_twpayment_payment._LOCAL_LANG.de.transaction.show.description (
+<p>Es ist großartig, dass Du eine Stuhlpatenschaft in unserem Dojo übernehmen möchtest, und wir bedanken uns an dieser Stelle nochmals herzlich bei Dir! Wir haben Dich auf diese Seite geführt, weil Du Dich für die Zahlung per Kreditkarte entschieden hast. Wir bitten Dich nun, den Vorgang abzuschließen.</p>
+<p>Wir haben die folgende Transaktion für Dich vorbereitet. Im nächsten Schritt wirst Du um Angaben zu deiner Kreditkarte gebeten. Deine Zahlung wird über <a href="https://stripe.com" target="_blank">Stripe</a> verarbeitet werden.</p>
+)
